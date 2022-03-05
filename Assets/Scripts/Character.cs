8044467985d8af71d5b6ce953615201f00f0814b
Biruk Mengistu <mengi024@morris.umn.edu>
2022-03-04 22:32:14 -0600

    added animations and character movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum CharacterState { 
+        WALKING_UP, WALKING_DOWN,WALKING_LEFT,WALKING_RIGHT,
+        FACING_UP, FACING_DOWN, FACING_LEFT, FACING_RIGHT
+    }
+
+    public CharacterState currentState = CharacterState.FACING_DOWN;
+    private Animator animator;
+    public float speed = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentState = CharacterState.FACING_DOWN;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.W)) {
+            currentState = CharacterState.WALKING_UP;
+            transform.position = new Vector3(pos.x,pos.y + speed * Time.deltaTime,pos.z);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentState = CharacterState.WALKING_DOWN;
+            transform.position = new Vector3(pos.x, pos.y - speed * Time.deltaTime, pos.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentState = CharacterState.WALKING_LEFT;
+            transform.position = new Vector3(pos.x - speed * Time.deltaTime,pos.y, pos.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentState = CharacterState.WALKING_RIGHT;
+            transform.position = new Vector3(pos.x + speed * Time.deltaTime, pos.y, pos.z);
+        }
+
+
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
+            currentState = currentState ==
+                CharacterState.WALKING_UP ? CharacterState.FACING_UP :
+                currentState == CharacterState.WALKING_DOWN ? CharacterState.FACING_DOWN :
+                currentState == CharacterState.WALKING_LEFT ? CharacterState.FACING_LEFT :
+                currentState == CharacterState.WALKING_RIGHT ? CharacterState.FACING_RIGHT : currentState;
+        }
+        print(currentState);
+
+        animator.SetInteger("CharacterState", (int)currentState);
+    }
+
+}
