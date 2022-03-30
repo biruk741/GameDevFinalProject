a614ddb3184d4124828844d81a7472f4ae462ba9
Isabelle Hjelden <hjeld013@morris.umn.edu>
2022-03-30 12:40:25 -0500

    Main Menu
    Things that are not completed: Audio, and controls page, and main menu is not linked to anything. Credits page is a place holder and is not completed.

---
+++ Assets/Main Menu Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button ControlsOpenButton;
+    [SerializeField] private Button ControlsClosedButton;
+    [SerializeField] private GameObject ControlsUI;
+    [SerializeField] private Button ExitButton;
+
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        ControlsOpenButton.onClick.AddListener(() => { ControlsUI.SetActive(true); });
+        ControlsClosedButton.onClick.AddListener(() => { ControlsUI.SetActive(false); });
+        PlayButton.onClick.AddListener(() => LoadingScreen.LoadScene(""));
+    }
+}
