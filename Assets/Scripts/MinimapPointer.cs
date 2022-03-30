88a402b7b4390b78778e5094161bd492ccba55f6
Biruk Mengistu <mengi024@morris.umn.edu>
2022-03-30 12:46:37 -0500

    fixed minimap pointer

--- Assets/Scripts/MinimapPointer.cs
+++ Assets/Scripts/MinimapPointer.cs
@@ -20,18 +20,15 @@ public class MinimapPointer : MonoBehaviour
         destinationProvided = destination != null;
         if (destinationProvided) {
             Vector3 vector = (player.position - destination.position).normalized;
-            float angleInRadians = Mathf.Atan(vector.y/vector.x);
+            float angleInRadians = Mathf.Atan2(vector.y, vector.x);
             double angleInDegrees = (180 / System.Math.PI) * angleInRadians;
             Quaternion rotation = transform.rotation;
 
             print("vector.x = " + vector.x + " and vector.y = " + vector.y + " y/x = " + (vector.x/vector.y));
 
             //rotation.z = 1-Mathf.Atan(vector.y / vector.x) *Mathf.Rad2Deg / 90f;
-            // rotation.z = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
-            rotation.z = vector.x;
-            rotation.w = -vector.y;
-                        //rotation.w = vector.y < 0 ? -vector.y: vector.y;
-            transform.rotation = rotation;
+            //rotation.w = vector.y < 0 ? -vector.y: vector.y;
+            transform.localRotation = Quaternion.Euler(0, 0, (float)angleInDegrees + 90);
         }
     }
 }
